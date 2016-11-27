3505841ced51df2b31f7784b883ce31fe4e8448d
MarcinSpin <marcin.rozycki86@gmail.com>
2016-11-27 20:26:26 +0100

    nowe plik js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// punkt1
+
+var a = prompt('Podaj zmienna a');
+var b = prompt('Podaj zmienna b');
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log (value);
+if (value > 0) 
+	{
+	alert ('wynik dodadni');
+	}
+else if (value == 0)
+	{
+	alert('wynik rowny 0');
+	}
+else 
+	{
+	alert ('wynik ujemny');
+	}
+
+
+
+//punkt2
+
