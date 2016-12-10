18d6e263a696d69a42a16143a48cb0c35b4d5e43
MarcinSpin <marcin.rozycki86@gmail.com>
2016-12-10 23:25:37 +0100

    Ciągi

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+console.log(text.length/2);
+	
+var animal = 'Zielone Słonie';
+var animal = animal.toUpperCase();
+
+var propertext = text.replace('Papugi',animal);
+console.log(propertext);
+
+
+console.log(propertext.substring(0,41));
