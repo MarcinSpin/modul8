984e54d032f8f1322db6d533beeefcb107a6d95d
Nemethe <nowakmarceli@gmail.com>
2016-12-20 21:13:27 +0100

    poprawki wizualne kodu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(function(){ 
 	var carouselList = $("#carousel");
 	setInterval(function(){
-		carouselList.animate({'marginLeft':-400}, 500, 
-			function(){$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});;
-})
+		carouselList.animate({'marginLeft':-400}, 500, function() {
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+		})
 	},3000);
 });
