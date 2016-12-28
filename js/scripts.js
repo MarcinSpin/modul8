91d44c4956385f515fa8e449a8ac9f655a07d3c8
Nemethe <nowakmarceli@gmail.com>
2016-12-28 09:50:54 +0100

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
-$('body').append(this.$element);
 
 function Button(text) {
     this.text = text || 'Hello';
-    var btn1 = new Button('Hello!');
-btn1.create(); 
+    this.create(); // do metody create odwołujesz się bezpośrednio poprzez this
 }
-Button.prototype = {
-        create: function() {
-            var self = this;
-            this.$element = $('<button>');
-            this.$element.text(this.text);
-            this.$element.click(function() {
-                alert(self.text);
-            });
-        }
 
-    }
+Button.prototype.create = function() {
+    var self = this;
+    this.$element = $('<button>');
+    this.$element.text(this.text);
+    this.$element.click(function() {
+        alert(self.text);
+    });
+    $('body').append(this.$element);
+};
 
 
+var btn1 = new Button('Hello!'); // nie możesz wywoływać klasy bezpośrednio wewnątrz samej siebie, doprowadziło by to do nieskończonej pętli
